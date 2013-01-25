31a709890bc3eb4bfce069b013cf90dcf9f914c2
abdualrhman.ali <abdualrhman1410@gmail.com>
2013-01-25 17:31:00 +0300

    try comments on files

--- try.php
+++ try.php
@@ -1,3 +1,21 @@
-<?
-echo 'try upload file in github.com :D';
-?>
+From 65df2281e4d75f57f0177489c2b6aee18a39a881 Mon Sep 17 00:00:00 2001
+From: "abdualrhman.ali" <abdualrhman1410@gmail.com>
+Date: Sat, 12 Jan 2013 01:35:33 -0800
+Subject: [PATCH] Create README.md
+
+---
+ README.md |    2 ++
+ 1 file changed, 2 insertions(+)
+ create mode 100644 README.md
+
+diff --git a/README.md b/README.md
+new file mode 100644
+index 0000000..7c78793
+--- /dev/null
++++ b/README.md
+@@ -0,0 +1,2 @@
++test1
++=====
+-- 
+1.7.10.4
+
